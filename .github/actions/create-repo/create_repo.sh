761a95f7c48c99c6782d6d9172f06dd599e97fe5
Marcos Cianci <mlopesci@emeal.nttdata.com>
2022-09-14 20:14:52 -0300

    kknn

--- .github/actions/create-repo/create_repo.sh
+++ .github/actions/create-repo/create_repo.sh
@@ -2,16 +2,15 @@
 
 GITHUB_CLI=$(which gh)
 
-echo $GH_TOKEN
-export GH_TOKEN=$GH_TOKEN
 
 # AUTHENTICATE GITHUB
 $GITHUB_CLI auth login
 
-if [ $TEMPLATE == "nttdata-template-empty-private" ]; then
-    $GITHUB_CLI repo create --template Digital-Architecture/${TEMPLATE} Digital-Architecture/${depart}-${app} --private 
+if [ "$TEMPLATE" == "nttdata-template-empty-private" ] 
+then
+    $GITHUB_CLI repo create --template Digital-Architecture/${TEMPLATE} Digital-Architecture/${depart}-${app} --private
 else
-    $GITHUB_CLI repo create --template Digital-Architecture/${TEMPLATE} Digital-Architecture/${depart}-${app} --public
+   $GITHUB_CLI repo create --template Digital-Architecture/${TEMPLATE} Digital-Architecture/${depart}-${app} --public
 fi
 
 
