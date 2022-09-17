a23968c058845c414656b2d421b9c55f085f862b
Marcos Cianci <mlopesci@emeal.nttdata.com>
2022-09-17 15:24:24 -0300

    update script inset docs

--- .github/actions/create-repo/create_repo.sh
+++ .github/actions/create-repo/create_repo.sh
@@ -9,8 +9,17 @@ $GITHUB_CLI auth login
 if [ "$TEMPLATE" == "nttdata-template-empty-private" ] 
 then
     $GITHUB_CLI repo create --template Digital-Architecture/${TEMPLATE} Digital-Architecture/${DEPART}-${APP} --private
+
+elif [ "$TEMPLATE" == "nttdata-template-empty-private" ]
+then
+    $GITHUB_CLI repo create --template Digital-Architecture/${TEMPLATE} Digital-Architecture/${DEPART}-${APP} --public
+
+elif [ "$TEMPLATE" == "nttdata-template-docs-hugo" ]
+then
+    $GITHUB_CLI repo create --template Digital-Architecture/${TEMPLATE} Digital-Architecture/${DEPART}${APP}.github.io --public
 else
-   $GITHUB_CLI repo create --template Digital-Architecture/${TEMPLATE} Digital-Architecture/${DEPART}-${APP} --public
+    echo "Template invalid!!!!"
+
 fi
 
 
