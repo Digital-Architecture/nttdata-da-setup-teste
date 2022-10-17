ec8fbbe29006c76fb3442c176c12bf4f301a8a9a
Marcos Cianci <mlopesci@emeal.nttdata.com>
2022-10-17 00:18:33 -0300

    update

--- .github/actions/create-repo/create_repo.sh
+++ .github/actions/create-repo/create_repo.sh
@@ -6,6 +6,12 @@ GITHUB_CLI=$(which gh)
 $GITHUB_CLI auth login
 
 
+# Function
+check_template() {
+    $GITHUB_CLI repo list |grep $1
+}
+
+
 # Create Repo
 
 [ "$TEMPLATE" == "nttdata-template-empty-private" ] && $GITHUB_CLI repo create --template Digital-Architecture/${TEMPLATE} Digital-Architecture/${DEPART}-${APP} --private
@@ -13,9 +19,9 @@ $GITHUB_CLI auth login
 
 [ "$TEMPLATE" == "nttdata-template-docs-hugo" ] && $GITHUB_CLI repo create --template Digital-Architecture/${TEMPLATE} Digital-Architecture/${DEPART}${APP} --public
 
-[ "$TEMPLATE" == "nttdata-template-modules-terraform" ] && $GITHUB_CLI repo create --template Digital-Architecture/${TEMPLATE} Digital-Architecture/terraform-modules-aws-${MODULO} --public --include-all-branches
+[ "$TEMPLATE" == "nttdata-template-modules-terraform" ] || !check_template Digital-Architecture/terraform-modules-aws-${MODULO} && $GITHUB_CLI repo create --template Digital-Architecture/${TEMPLATE} Digital-Architecture/terraform-modules-aws-${MODULO} --public --team Digital-Architecture/SRE
 
-[ "$TEMPLATE" == "nttdata-template-infra-terraform" ] && $GITHUB_CLI repo create --template Digital-Architecture/${TEMPLATE} Digital-Architecture/${PROJETO}-${APP}-infra-terraform --public --include-all-branches
+[ "$TEMPLATE" == "nttdata-template-infra-terraform" ] || !check_template Digital-Architecture/${PROJETO}-${APP}-infra-terraform && $GITHUB_CLI repo create --template Digital-Architecture/${TEMPLATE} Digital-Architecture/${PROJETO}-${APP}-infra-terraform --public --team Digital-Architecture/SRE
 
 
 
